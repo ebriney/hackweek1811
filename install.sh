fa55425b6b317dd83020739997f06f078abb1d65
ebriney <emmanuel.briney@docker.com>
2018-11-09 16:51:18 +0100

    services
    Signed-off-by: ebriney <emmanuel.briney@docker.com>

--- install.sh
+++ install.sh
@@ -5,10 +5,13 @@ INSTALLDIR=/usr/local/bin
 rm -rf "${TMPDIR}"
 mkdir "${TMPDIR}"
 curl -fslL --retry 10 https://github.com/containerd/containerd/releases/download/v1.2.0/containerd-1.2.0.linux-amd64.tar.gz | tar xzf - -C "${TMPDIR}"
+curl -fslL --retry 10 -o "${TMPDIR}"/bin/runc https://github.com/opencontainers/runc/releases/download/v1.0.0-rc5/runc.amd64
+curl -fslL --retry 10 -o "${TMPDIR}"/bin/restartd https://download-stage.docker.com/win/restartd
 chmod +x "${TMPDIR}"/bin/*
 
-curl -fslL --retry 10 -o "${TMPDIR}"/runc https://github.com/opencontainers/runc/releases/download/v1.0.0-rc5/runc.amd64
-chmod +x "${TMPDIR}"/runc
+# install dependencies
+sudo cp -f "${TMPDIR}"/bin/* /usr/local/bin
 
-sudo cp "${TMPDIR}"/runc "${INSTALLDIR}"
-sudo cp "${TMPDIR}"/bin/* /usr/local/bin
+#install services
+sudo cp systemd/* /lib/systemd/system
+sudo systemctl daemon-reload
